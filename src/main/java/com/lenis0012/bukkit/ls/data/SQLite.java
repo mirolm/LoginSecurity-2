d3a2106afc155814e646a3efdf27153644e8049e
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-05-16 09:44:53 +0300

    Update SQLite.java

--- src/main/java/com/lenis0012/bukkit/ls/data/SQLite.java
+++ src/main/java/com/lenis0012/bukkit/ls/data/SQLite.java
@@ -15,11 +15,11 @@ import com.lenis0012.bukkit.ls.encryption.EncryptionType;
 
 public class SQLite implements DataManager {
 	public static boolean IS_CONVERTING = false;
-	
+
 	private final Logger log = Logger.getLogger("Minecraft.LoginSecurity");
 	private File file;
 	private Connection con;
-	
+
 	public SQLite(File file) {
 		this.file = file;
 		File dir = file.getParentFile();
@@ -31,24 +31,24 @@ public class SQLite implements DataManager {
 				log.log(Level.SEVERE, "Failed to create file", e);
 			}
 		}
-		
+
 		try {
 			Class.forName("org.sqlite.JDBC");
 		} catch (ClassNotFoundException e) {
 			log.log(Level.SEVERE, "Failed to load SQLite driver", e);
 		}
-		
+
 		this.openConnection();
 		UUIDConverter uuidConverter = new UUIDConverter(this, log, "users");
 		uuidConverter.convert();
 	}
-	
+
 	@Override
 	public void openConnection() {
 		try {
 			this.con = DriverManager.getConnection("jdbc:sqlite:" + file.getPath());
 			Statement st = con.createStatement();
-			
+
 			st.setQueryTimeout(30);
 			st.executeUpdate("CREATE TABLE IF NOT EXISTS users (unique_user_id VARCHAR(130) NOT NULL UNIQUE,password VARCHAR(300) NOT NULL,encryption INT,ip VARCHAR(130) NOT NULL);");
 		} catch (SQLException e) {
@@ -70,7 +70,7 @@ public class SQLite implements DataManager {
 	public boolean isRegistered(String uuid) {
 		try {
 			PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE unique_user_id=?;");
-			ps.setString(1, uuid);
+			ps.setString(1, uuid.replaceAll("-", ""));
 			ResultSet result = ps.executeQuery();
 			return result.next();
 		} catch(SQLException e) {
@@ -83,7 +83,7 @@ public class SQLite implements DataManager {
 	public void register(String uuid, String password, int encryption, String ip) {
 		try {
 			PreparedStatement ps = con.prepareStatement("INSERT INTO users(unique_user_id,password,encryption,ip) VALUES(?,?,?,?);");
-			ps.setString(1, uuid);
+			ps.setString(1, uuid.replaceAll("-", ""));
 			ps.setString(2, password);
 			ps.setInt(3, encryption);
 			ps.setString(4, ip);
@@ -99,7 +99,7 @@ public class SQLite implements DataManager {
 			PreparedStatement ps = con.prepareStatement("UPDATE users SET password=?,encryption=? WHERE unique_user_id=?;");
 			ps.setString(1, password);
 			ps.setInt(2, encryption);
-			ps.setString(3, uuid);
+			ps.setString(3, uuid.replaceAll("-", ""));
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			log.log(Level.SEVERE, "Failed to update user password", e);
@@ -111,7 +111,7 @@ public class SQLite implements DataManager {
 		try {
 			PreparedStatement ps = con.prepareStatement("UPDATE users SET ip=? WHERE unique_user_id=?;");
 			ps.setString(1, ip);
-			ps.setString(2, uuid);
+			ps.setString(2, uuid.replaceAll("-", ""));
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			log.log(Level.SEVERE, "Failed to update user ip", e);
@@ -122,7 +122,7 @@ public class SQLite implements DataManager {
 	public String getPassword(String uuid) {
 		try {
 			PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE unique_user_id=?;");
-			ps.setString(1, uuid);
+			ps.setString(1, uuid.replaceAll("-", ""));
 			ResultSet result = ps.executeQuery();
 			if(result.next())
 				return result.getString("password");
@@ -138,7 +138,7 @@ public class SQLite implements DataManager {
 	public int getEncryptionTypeId(String uuid) {
 		try {
 			PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE unique_user_id=?;");
-			ps.setString(1, uuid);
+			ps.setString(1, uuid.replaceAll("-", ""));
 			ResultSet result = ps.executeQuery();
 			if(result.next())
 				return result.getInt("encryption");
@@ -154,7 +154,7 @@ public class SQLite implements DataManager {
 	public String getIp(String uuid) {
 		try {
 			PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE unique_user_id=?;");
-			ps.setString(1, uuid);
+			ps.setString(1, uuid.replaceAll("-", ""));
 			ResultSet result = ps.executeQuery();
 			if(result.next())
 				return result.getString("ip");
@@ -170,7 +170,7 @@ public class SQLite implements DataManager {
 	public void removeUser(String uuid) {
 		try {
 			PreparedStatement ps = con.prepareStatement("DELETE FROM users WHERE unique_user_id=?;");
-			ps.setString(1, uuid);
+			ps.setString(1, uuid.replaceAll("-", ""));
 			ps.executeUpdate();
 		} catch(SQLException e) {
 			log.log(Level.SEVERE, "Failed to remove user", e);
