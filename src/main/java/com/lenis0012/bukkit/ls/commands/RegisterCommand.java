a366b207182539b3b48bb428856ea2f4efd5eedf
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-05-16 19:00:38 +0300

    Update RegisterCommand.java

--- src/main/java/com/lenis0012/bukkit/ls/commands/RegisterCommand.java
+++ src/main/java/com/lenis0012/bukkit/ls/commands/RegisterCommand.java
@@ -29,7 +29,7 @@ public class RegisterCommand implements CommandExecutor {
 		}
 		if (args.length < 1) {
 			player.sendMessage(ChatColor.RED + Lang.INVALID_ARGS.toString());
-			player.sendMessage(ChatColor.RED + Lang.USAGE + cmd.getUsage());
+			player.sendMessage(ChatColor.RED + Lang.USAGE.toString() + cmd.getUsage());
 			return true;
 		}
 
