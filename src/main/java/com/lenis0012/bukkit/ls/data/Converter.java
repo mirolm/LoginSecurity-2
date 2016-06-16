dd9ff563bddbc026a2514ff1959843127f2df21a
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-06-16 19:39:26 +0300

    Update Converter.java

--- src/main/java/com/lenis0012/bukkit/ls/data/Converter.java
+++ src/main/java/com/lenis0012/bukkit/ls/data/Converter.java
@@ -18,8 +18,7 @@ import com.lenis0012.bukkit.ls.util.EncryptionUtil;
 public class Converter {
 	public static enum FileType {
 		YAML,
-		SQLite,
-		OldToNewMySQL;
+		SQLite;
 	}
 	
 	private FileType type;
@@ -39,7 +38,7 @@ public class Converter {
 			if(config.getConfigurationSection("password.password") != null) {
 				Set<String> set = config.getConfigurationSection("password.password").getKeys(false);
 				for(String user : set) {
-					String pass = config.getString("password.password."+user);
+					String pass = config.getString("password.password." + user);
 					if(!md5)
 						pass = EncryptionUtil.getMD5(pass);
 					plugin.data.register(user, pass, 1, RandomStringUtils.randomAscii(25));
@@ -63,28 +62,7 @@ public class Converter {
 				file.delete();
 			} catch(SQLException e) {
 				System.out.println("[LoginSecurity] FAILED CONVERTING FROM SQLITE TO MYSQL");
-				log.warning("[LoginSecurity] "+e.getMessage());
-			}
-		} else if(type == FileType.OldToNewMySQL) {
-			try {
-				MySQL from = new MySQL(plugin.getConfig(), "passwords");
-				from.openConnection();
-				boolean shouldEncrypt = plugin.getConfig().getBoolean("options.use-MD5 Enryption", true);
-				ResultSet result = from.getAllUsers();
-				while(result.next()) {
-					String user = result.getString("username");
-					if(!plugin.data.isRegistered(user)) {
-						String pass = result.getString("password");
-						if(!shouldEncrypt)
-							pass = EncryptionUtil.getMD5(pass);
-						plugin.data.register(user, pass, 1, RandomStringUtils.randomAscii(25));
-					}
-				}
-				from.dropTable("passwords");
-				from.closeConnection();
-			} catch(SQLException e) {
-				System.out.println("[LoginSecurity] FAILED CONVERTING FROM SQLITE TO MYSQL");
-				log.warning("[LoginSecurity] "+e.getMessage());
+				log.warning("[LoginSecurity] " + e.getMessage());
 			}
 		}
 	}
