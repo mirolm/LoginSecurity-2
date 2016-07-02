aac06ae58a9d5fa2776ab7485e2367b5ce30056d
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-07-03 00:50:43 +0300

    Update PasswordManager.java

--- src/main/java/com/lenis0012/bukkit/ls/encryption/PasswordManager.java
+++ src/main/java/com/lenis0012/bukkit/ls/encryption/PasswordManager.java
@@ -2,14 +2,16 @@ package com.lenis0012.bukkit.ls.encryption;
 
 import com.lenis0012.bukkit.ls.LoginSecurity;
 import com.lenis0012.bukkit.ls.data.DataManager;
+import com.lenis0012.bukkit.ls.data.LoginData;
 
 public class PasswordManager {
 	public static boolean checkPass(String uuid, String password) {
 		LoginSecurity plugin = LoginSecurity.instance;
 		DataManager data = plugin.data;
-		String realPass = data.getPassword(uuid);
-		int type = data.getEncryptionTypeId(uuid);
-		EncryptionType etype = EncryptionType.fromInt(type);
-		return etype.checkPass(password, realPass);
+
+		LoginData login = data.getData(uuid);		
+		EncryptionType etype = EncryptionType.fromInt(login.encryption);
+
+		return etype.checkPass(password, login.password);
 	}
 }
