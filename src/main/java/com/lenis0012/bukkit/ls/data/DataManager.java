8e135c4fc69a306db0accd405d45c72ffc4dc79f
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-07-03 00:49:01 +0300

    Update DataManager.java

--- src/main/java/com/lenis0012/bukkit/ls/data/DataManager.java
+++ src/main/java/com/lenis0012/bukkit/ls/data/DataManager.java
@@ -3,87 +3,73 @@ package com.lenis0012.bukkit.ls.data;
 import java.sql.ResultSet;
 
 public interface DataManager {
-	
+
 	/**
 	 * Open SQL connection
 	 */
 	public void openConnection();
-	
+
 	/**
 	 * Close SQL connection
 	 */
 	public void closeConnection();
-	
+
 	/**
 	 * Check if a player is registered
-	 * 
+	 *
 	 * @param user Username
 	 * @return User registered?
 	 */
 	public boolean isRegistered(String user);
-	
+
 	/**
 	 * Register a user
-	 * 
-	 * @param user Username
-	 * @param password Password
-	 * @param encryption Encryption type id
-	 * @param ip User address
+	 *
+	 * @param login LoginData
 	 */
-	public void register(String user, String password, int encryption, String ip);
-	
+	public void register(LoginData login);
+
 	/**
 	 * Update a player's password
-	 * 
-	 * @param user Username
-	 * @param password New password
-	 * @param encryption Encryption type id
+	 *
+	 * @param login LoginData
 	 */
-	public void updatePassword(String user, String password, int encryption);
-	
+	public void updatePassword(LoginData login);
+
 	/**
 	 * Update a user ip
 	 * 
-	 * @param user Username
-	 * @param ip New address
+	 * @param login LoginData
 	 */
-	public void updateIp(String user, String ip);
-	
+	public void updateIp(LoginData login);
+
 	/**
-	 * Get a user password
-	 * 
-	 * @param user Username
-	 * @return Password
+	 * Get user stored data
+	 *
+	 * @param uuid Player UUID
+	 * @return LoginData
 	 */
-	public String getPassword(String user);
-	
-	/**
-	 * Get a user encryption type
-	 * 
-	 * @param user Username
-	 * @return Encryption type id
-	 */
-	public int getEncryptionTypeId(String user);
-	
-	/**
-	 * Get a user ip address
-	 * 
-	 * @param user Username
-	 * @return Encryption type id
-	 */
-	public String getIp(String user);
-	
+	public LoginData getData(String uuid);
+
 	/**
 	 * Remove a user from the database
-	 * 
+	 *
 	 * @param user Username
 	 */
 	public void removeUser(String user);
-	
+
 	/**
 	 * Get all registered users
-	 * 
+	 *
 	 * @return All registered users
 	 */
 	public ResultSet getAllUsers();
+
+	/**
+	 * Parse single used row
+	 *
+	 * @param data ResultSet
+	 * @return LoginData
+	 */
+	public LoginData parseData(ResultSet data);
 }
