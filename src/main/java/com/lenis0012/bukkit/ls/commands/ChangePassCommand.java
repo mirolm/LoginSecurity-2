175a5766679b53ccc01a173a161043cc13215107
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-07-04 20:44:37 +0300

    Update ChangePassCommand.java

--- src/main/java/com/lenis0012/bukkit/ls/commands/ChangePassCommand.java
+++ src/main/java/com/lenis0012/bukkit/ls/commands/ChangePassCommand.java
@@ -25,7 +25,7 @@ public class ChangePassCommand implements CommandExecutor {
 		Player player = (Player) sender;
 		String uuid = player.getUniqueId().toString();
 
-		if (!plugin.data.isRegistered(uuid)) {
+		if (!plugin.data.checkUser(uuid)) {
 			player.sendMessage(ChatColor.RED + Lang.NOT_REG.toString());
 			return true;
 		}
