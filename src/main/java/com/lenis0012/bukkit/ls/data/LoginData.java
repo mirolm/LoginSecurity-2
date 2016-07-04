bb6fabeaf6e6986bda9eb2ef4c26ac44acf2f3fc
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-07-04 19:12:13 +0300

    Update LoginData.java

--- src/main/java/com/lenis0012/bukkit/ls/data/LoginData.java
+++ src/main/java/com/lenis0012/bukkit/ls/data/LoginData.java
@@ -1,23 +1,25 @@
 package com.lenis0012.bukkit.ls.data;
-	
+
 public class LoginData {
 	public final String uuid;
 	public final String password;
 	public final int encryption;
 	public final String ipaddr;
-	
+
 	public LoginData(String uuid, String password, int encryption, String ipaddr) {
 		this.uuid = uuid;
 		this.password = password;
 		this.encryption = encryption;
 		this.ipaddr = ipaddr;
 	}
+
 	public LoginData(String uuid, String password, int encryption) {
 		this.uuid = uuid;
 		this.password = password;
 		this.encryption = encryption;
 		this.ipaddr = null;
 	}
+
 	public LoginData(String uuid, String ipaddr) {
 		this.uuid = uuid;
 		this.password = null;
