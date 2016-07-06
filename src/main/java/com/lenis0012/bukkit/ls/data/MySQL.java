81c61eae1e6c7551732a006423c0ed6f2f08ca1e
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-07-06 20:59:57 +0300

    Update MySQL.java

--- src/main/java/com/lenis0012/bukkit/ls/data/MySQL.java
+++ src/main/java/com/lenis0012/bukkit/ls/data/MySQL.java
@@ -6,15 +6,15 @@ public class MySQL extends SQL {
 	public MySQL(FileConfiguration config) {
 		super("com.mysql.jdbc.Driver");
 
-		String host = config.getString("MySQL.host", "localhost");
-		String port = String.valueOf(config.getInt("MySQL.port", 3306));
-		String database = config.getString("MySQL.database", "");
-		String user = config.getString("MySQL.username", "");
-		String pass = config.getString("MySQL.password", "");
-		String table = config.getString("MySQL.prefix", "") + "users";
+		final String host = config.getString("MySQL.host", "localhost");
+		final String port = String.valueOf(config.getInt("MySQL.port", 3306));
+		final String database = config.getString("MySQL.database", "");
+		final String user = config.getString("MySQL.username", "");
+		final String pass = config.getString("MySQL.password", "");
+		final String table = config.getString("MySQL.prefix", "") + "users";
 
-		String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+		final String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?user=" + user + "&password=" + pass;
 
-		initConn(table, url, user, pass);
+		initConn(table, url);
 	}
 }
