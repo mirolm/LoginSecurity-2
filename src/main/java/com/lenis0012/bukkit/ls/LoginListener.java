44659195da4eb075799f2c4371228e4edf8b8007
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-11-23 22:59:01 +0200

    Update LoginListener.java

--- src/main/java/com/lenis0012/bukkit/ls/LoginListener.java
+++ src/main/java/com/lenis0012/bukkit/ls/LoginListener.java
@@ -8,7 +8,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
