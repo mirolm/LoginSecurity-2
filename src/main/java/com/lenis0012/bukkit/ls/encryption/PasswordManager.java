19a81c41d99628fa4fe0d5658ab73379b9755be6
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-12-06 22:26:47 +0200

    Update PasswordManager.java

--- src/main/java/com/lenis0012/bukkit/ls/encryption/PasswordManager.java
+++ src/main/java/com/lenis0012/bukkit/ls/encryption/PasswordManager.java
@@ -17,6 +17,6 @@ public class PasswordManager {
 	
 	public static boolean validPass(String password) {
 		// at least 4 chars 1 letter 1 number
-		return password.matches("^(?=.*[a-zA-Z])(?=.*\d).{4,}+$");	
+		return password.matches("^(?=.*[a-zA-Z])(?=.*\\d).{4,}+$");	
 	}
 }
