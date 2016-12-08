4fe06b70f34e72b10ed0130fb2100944b25c1569
Miroslav Marchev <mirolm@users.noreply.github.com>
2016-12-08 20:47:48 +0200

    Update LoginCommand.java

--- src/main/java/com/lenis0012/bukkit/ls/commands/LoginCommand.java
+++ src/main/java/com/lenis0012/bukkit/ls/commands/LoginCommand.java
@@ -19,7 +19,6 @@ public class LoginCommand implements CommandExecutor {
 		Logger logger = plugin.getLogger();
 		
 		if(!(sender instanceof Player)) {
-			sender.sendMessage(Lang.MUST_BE_PLAYER.toString());
 			return true;
 		}
 
