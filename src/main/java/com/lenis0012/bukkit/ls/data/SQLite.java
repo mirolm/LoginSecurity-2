b78cdd1022d3ba1c18eda9bf77f845d14deaed20
Miroslav Marchev <mirolm@users.noreply.github.com>
2017-01-27 20:23:07 +0200

    Update SQLite.java

--- src/main/java/com/lenis0012/bukkit/ls/data/SQLite.java
+++ src/main/java/com/lenis0012/bukkit/ls/data/SQLite.java
@@ -1,15 +1,22 @@
 package com.lenis0012.bukkit.ls.data;
 
 import java.io.File;
+import com.zaxxer.hikari.HikariConfig;
 
 public class SQLite extends SQL {
 	public SQLite(File file) {
 		super("org.sqlite.JDBC");
 
 		String table = "users";
+        
+		HikariConfig config = new HikariConfig();
+		
+        	config.setDriverClassName("org.sqlite.JDBC");
+	        config.setUsername("");
+        	config.setPassword("");
+            	config.setMaximumPoolSize(1);
+        	config.setJdbcUrl("jdbc:sqlite://" + file.getAbsolutePath());
 
-		String url  = "jdbc:sqlite://" + file.getAbsolutePath();
-
-		initConn(table, url);
+		initConn(table, config);
 	}
 }
