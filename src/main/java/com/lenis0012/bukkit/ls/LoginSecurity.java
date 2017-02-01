76f021c345295d9d47f43bb4f85add2562df53d3
Miroslav Marchev <mirolm@users.noreply.github.com>
2017-02-01 23:06:42 +0200

    Update LoginSecurity.java

--- src/main/java/com/lenis0012/bukkit/ls/LoginSecurity.java
+++ src/main/java/com/lenis0012/bukkit/ls/LoginSecurity.java
@@ -17,7 +17,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.ChatColor;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
