be48048b1a62d4d4a72c4bb19ee4a07dc1cf7e9c
mirolm <mirolm@ieti.eu>
2021-11-30 23:14:37 +0200

    Spigot-API 1.18

--- src/main/java/com/lenis0012/loginsecurity/command/LoginCommand.java
+++ src/main/java/com/lenis0012/loginsecurity/command/LoginCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class LoginCommand implements CommandExecutor {
     private final LoginSecurity plugin;
@@ -14,7 +15,7 @@ public class LoginCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 1) {
